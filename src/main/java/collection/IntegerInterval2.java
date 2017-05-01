682e75758444a8b023f173d61efb8f563fb2e266
bmeritter <bme_ritter@foxmail.com>
2017-05-01 18:34:00 +0800

    IntegerInterval2 spec

---
+++ src/main/java/collection/IntegerInterval2.java
@@ -0,0 +1,11 @@
+package collection;
+
+/**
+ * Created by ritter on 17-5-1.
+ */
+public class IntegerInterval2 {
+  public int[] get_integer_interval(int number_1, int number_b) {
+
+    return null;
+  }
+}
---
+++ src/test/java/collection/IntegerInterval2Test.java
@@ -0,0 +1,43 @@
+package collection;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+/**
+ * Created by ritter on 17-5-1.
+ */
+public class IntegerInterval2Test {
+  @Test
+  public void should_return_integer_interval_1() {
+    IntegerInterval2 integerInterval2 = new IntegerInterval2();
+    int[] collection_a = {2, 4, 6, 8, 10};
+    int[] result = integerInterval2.get_integer_interval(1, 10);
+    assertArrayEquals(result, collection_a);
+  }
+
+  @Test
+  public void should_return_integer_interval_2() {
+    IntegerInterval2 integerInterval2 = new IntegerInterval2();
+    int[] collection_a = {10, 8, 6, 4, 2};
+    int[] result = integerInterval2.get_integer_interval(10, 1);
+    assertArrayEquals(result, collection_a);
+  }
+
+  @Test
+  public void should_return_integer_interval_3() {
+    IntegerInterval2 integerInterval2 = new IntegerInterval2();
+    int[] collection_a = {10};
+    int[] result = integerInterval2.get_integer_interval(10, 10);
+    assertArrayEquals(result, collection_a);
+  }
+
+  @Test
+  public void should_return_integer_interval_4() {
+    IntegerInterval2 integerInterval2 = new IntegerInterval2();
+    int[] collection_a = {};
+    int[] result = integerInterval2.get_integer_interval(5, 5);
+    assertArrayEquals(result, collection_a);
+  }
+
+}

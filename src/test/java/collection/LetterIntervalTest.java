d21433b177f2675372613082b15c3d43598b333b
bmeritter <bme_ritter@foxmail.com>
2017-05-02 16:50:39 +0800

    add letter interval test 2、3

--- src/test/java/collection/LetterIntervalTest.java
+++ src/test/java/collection/LetterIntervalTest.java
@@ -16,4 +16,21 @@ public class LetterIntervalTest {
     String[] result = letterInterval.get_letter_interval(1, 5);
     assertArrayEquals(result, collection_a);
   }
+
+  @Test
+  public void should_return_letter_interval_2() {
+    String[] collection_a = {"e", "d", "c", "b", "a"};
+    LetterInterval letterInterval = new LetterInterval();
+    String[] result = letterInterval.get_letter_interval(5, 1);
+    assertArrayEquals(result, collection_a);
+  }
+
+  @Test
+  public void should_return_letter_interval_3() {
+    String[] collection_a = {"e"};
+    LetterInterval letterInterval = new LetterInterval();
+    String[] result = letterInterval.get_letter_interval(5, 5);
+    assertArrayEquals(result, collection_a);
+  }
+
 }

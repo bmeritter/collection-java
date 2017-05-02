13131ab24b92d2ff9a54d0f38ae05bf788a84011
bmeritter <bme_ritter@foxmail.com>
2017-05-02 17:02:43 +0800

    letter interval

--- src/main/java/collection/LetterInterval.java
+++ src/main/java/collection/LetterInterval.java
@@ -5,8 +5,23 @@ package collection;
  */
 public class LetterInterval {
   public String[] get_letter_interval(int a, int b) {
+    String[] result;
+    int j = 0;
+    if (a < b) {
+      int len = b - a + 1;
+      result = new String[len];
+      for (int i = 97 + a - 1; i <= 97 + b - 1; ++i) {
+        result[j++] = String.valueOf((char) i);
+      }
+    } else {
+      int len = a - b + 1;
+      result = new String[len];
+      for (int i = a + 97 - 1; i >= 97 + b - 1; --i) {
+        result[j++] = String.valueOf((char) i);
+      }
+    }
 
 
-    return null;
+    return result;
   }
 }
